3258af10142cdf77c6f955b4d6feaffd89d72a98
ynishi789 <ynishi789@gmail.com>
2026-01-07 13:35:22 +0545

    variables

--- first.js
+++ first.js
@@ -1,3 +1,14 @@
-console.log('Nishi kumar yadav');
-fullname="Tony stark";
-console.log(fullname);
+//Global and Local variables declearation
+let name='apple';
+//Fuction defination
+function MyFunction(){
+    //Local varaibles declaration
+    let num=45;
+    //Display the value of Global variable
+    console.log(name);
+    //Display the value local variables 
+    console.log(num);
+
+}
+//Function call
+MyFunction();
